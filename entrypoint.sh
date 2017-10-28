80259ef32fa85e200e5212c27b156db49f6187f3
Silvio Fricke <silvio.fricke@gmail.com>
2017-10-28 10:20:00 +0200

    rework mpd.conf handle. user can provide own settings
    Signed-off-by: Silvio Fricke <silvio.fricke@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
 
 touch /mpd/cache/{tag_cache,state,sticker.sql} \
-  && chmod -R 0777 /mpd/cache \
-  && chown -R mpd /mpd/cache
+  && chmod -R 0777 /mpd/cache
+
+# overwrite system conf with user provided conf
+[ -e "/mpd/etc/mpd.conf" ] && cp /mpd/etc/mpd.conf /etc/mpd.conf
+
+# fallback: if no /mpd/etc/mpd.conf exists, copy current system mpd.conf to
+# host-space
+[ ! -e "/mpd/etc" ] && mkdir -p /mpd/etc
+[ ! -e "/mpd/etc/mpd.conf" ] && cp /etc/mpd.conf /mpd/etc/mpd.conf
 
 ## start
 exec mpd --no-daemon --stdout --verbose /etc/mpd.conf "$@"
